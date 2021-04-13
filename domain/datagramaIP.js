c951d38f855bb0cbf4187027ddb9d81ceb607754
Brayan Tabares <btabaresh@uqvirtual.edu.co>
2021-04-13 16:33:27 -0500

    Método Fragmentar

--- domain/datagramaIP.js
+++ domain/datagramaIP.js
@@ -2,76 +2,81 @@
  * 
  */
 
+//Datos previos
+const VERSION = 4;
+const LONG_ENCABEZADO =5;
+const SER_DIF = 0;
+const PROTOCOL_LIST=[new protocolObject("ICMP",1),new protocolObject("TCP",6),new protocolObject("UDP",17)];
+
+//Autogenerados
+const IDENTY = Math.round(Math.random()*65535);
+const TIME_LIFE = Math.round(Math.random()*255);
+
+//Campos entrantes - Inputs del html
+let MTU = 1500;
+let LEN_TOTAL = 3500;
+let PROTOCOL = PROTOCOL_LIST[0];
+let DIR_O = "192.168.0.1";
+let DIR_D = "192.168.0.2";
+
+//Datos calculados
+
+    //lista de fragmentos
+let FRAG_LIST = [];
+
+
+/**
+ * FUNCIONES
+ */
+
+
+function fragmento (len,df,mf,despl){
+    this.len=len;
+    this.df=df;
+    this.mf=mf;
+    this.despl=despl;
+    this.sum=0;
+    this.sum=calcularSumaComprobacion(this);
+    this.binString=generateBinString(this);
+    this.bin=generateBin(this);
+    this.hexa=generateHexa(this);
+}
+
 function protocolObject (nombre,numDecimal){
     this.nombre=nombre;
     this.numDecimal=numDecimal;
 }
 
-function transform(valor = 0,unidad = 2,tamano = 0){    
+function transform(valor,unidad = 2,tamano = 0){    
     let aux = valor.toString(unidad);
     if (tamano!=0){
         aux=aux.padStart(tamano,"0");
     }
     return aux;
-    
-}
-
-let protocolList=[new protocolObject("ICMP",1),new protocolObject("TCP",6),new protocolObject("UDP",17)];
-//let protocolList=[{nombre:"ICMP",numDecimal:1},{nombre:"TCP",numDecimal:6},{nombre:"UDP",numDecimal:17}];
-console.log(protocolList);
-
-//Campos entrantes
-let mtu = 1500;
-let lenTotal = 3500;
-let protocol = protocolList[0];
-let dirO = "192.168.0.1";
-let dirD = "192.168.0.2";
-
-
-//Autogenerados
-const identy = Math.round(Math.random()*65535);
-const timeLife = Math.round(Math.random()*255);
-
-//Datos previos
-const version = 4;
-const longEncabezado =5;
-const servDif = 0;
-
-//Datos calculados
-
-
-
-//Funcion para la creacion de fragmentos
-function fragmento (len,df,mf,despl,sum){
-    this.len=len;
-    this.df=df;
-    this.mf=mf;
-    this.despl=despl;
-    this.sum=sum;
 }
 
 function generateBinString (fragmento) {
-    let cadena=transform(version,2,4);
-    cadena+=transform(longEncabezado,2,4);
-    cadena+=transform(servDif,2,8);
+    let cadena=transform(VERSION,2,4);
+    cadena+=transform(LONG_ENCABEZADO,2,4);
+    cadena+=transform(SER_DIF,2,8);
     cadena+=transform(fragmento.len,2,16);
-    cadena+=transform(identy,2,16);
+    cadena+=transform(IDENTY,2,16);
     cadena+="0"+fragmento.df+fragmento.mf;
     cadena+=transform(fragmento.despl,2,13);
-    cadena+=transform(timeLife,2,8);
-    cadena+=transform(protocol.numDecimal,2,8);
+    cadena+=transform(TIME_LIFE,2,8);
+    cadena+=transform(PROTOCOL.numDecimal,2,8);
     cadena+=transform(fragmento.sum,2,16);
-    let ipO=dirO.split(".");
+    let ipO=DIR_O.split(".");
     ipO.forEach((e)=>{ cadena+=transform(Number(e),2,8)});
-    let ipD=dirD.split(".");
+    let ipD=DIR_D.split(".");
     ipD.forEach((e)=>{ cadena+=transform(Number(e),2,8)});
     return cadena;
 }
 
 function generateHexa (fragmento){
-    let arrayBinario=dividirStringEnArray(generateBinString(fragmento),4);
+    let arrayBinario=dividirStringEnArray(generateBinString(fragmento),8);
     let arrayHexa=new Array(arrayBinario.length);
-    arrayBinario.forEach((e,i,a)=>arrayHexa[i]=(parseInt(e,2)).toString(16));
+    arrayBinario.forEach((e,i)=>arrayHexa[i]=transform(parseInt(e,2),16,2));
     return arrayHexa;
 }
 
@@ -88,12 +93,64 @@ function dividirStringEnArray (cadena,numDeCaracteres){
     return newArray;
 }
 
+function calcularSumaComprobacion(fragmento){
+    let arrayHexa=generateHexa(fragmento);
+    let suma="0";
+    for(let i=0;i<arrayHexa.length;i+=2){
+        if(i!=10){
+            suma=sumarHexa(suma,arrayHexa[i]+arrayHexa[i+1]);
+            if(suma.length>4){
+                suma=sumarHexa(suma.substring(0,1),suma.substring(1));
+            }
+        }        
+    }
+    return parseInt(restarHexa("FFFF",suma),16);
+}
 
-//Funcion para la lista de fragmentos
+function sumarHexa(cadena1,cadena2){
+    let num1=parseInt(cadena1,16);
+    let num2=parseInt(cadena2,16);
+    return (num1+num2).toString(16);
+}
 
-let listFrag = [];
+function restarHexa(cadena1,cadena2){
+    let num1=parseInt(cadena1,16);
+    let num2=parseInt(cadena2,16);
+    return (num1-num2).toString(16);
+}
 
-document.write("<p><marquee><h1>HOOOOOOOOOOOOOLI</h1><marquee></p><p>MADAFAKA</p>")
+function fragmentar(mtu,lenTotal,protocol,dirO,dirD){
+    let tamanoEncabezado=20;
+    let numFragments=1;
+    let lastLen=lenTotal;
+    let boolFragmentar=false;
+    //Es necesario fragmentar?
+    if(mtu<lenTotal){
+        numFragments=Math.ceil((lenTotal-tamanoEncabezado)/(mtu-tamanoEncabezado));
+        lastLen=lenTotal-((mtu-tamanoEncabezado)*(numFragments-1));
+        boolFragmentar=true;
+    }
+    //Realizar fragmentación
+    FRAG_LIST=new Array(numFragments);
+    for (let i=0;i<numFragments;i++){
+        let len=mtu;
+        let mostFragments=0;
+        //Es el último fragmento?
+        if(boolFragmentar){            
+            if((i+1)<numFragments){
+                mostFragments=1;
+            }else{
+                len=lastLen;
+            }
+        }
+        let frag=new fragmento(len,0,mostFragments,(i*(mtu-tamanoEncabezado))/8)
+        FRAG_LIST[i]=frag;
+    }
+}
+
+
+
+//document.write("<p><marquee><h1>HOOOOOOOOOOOOOLI</h1><marquee></p><p>MADAFAKA</p>")
 
 /*
 document.write(identy+"\n");
@@ -102,5 +159,13 @@ document.write(Number("192").toString(2));
 document.write("192.168.0.1".split("."));
 document.write("192.168.0.1".split("."));
 */
-console.log(generateBin(new fragmento(lenTotal,0,0,0,0)));
-console.log(generateHexa(new fragmento(lenTotal,0,0,0,0)));
+
+fragmentoPrueba=new fragmento(LEN_TOTAL,0,0,0);
+//console.log(generateBin(fragmentoPrueba));
+//console.log(generateHexa(fragmentoPrueba));
+//console.log(fragmentoPrueba.binString);
+//console.log(fragmentoPrueba.bin);
+//console.log(fragmentoPrueba.hexa);
+fragmentar(MTU,LEN_TOTAL,PROTOCOL,DIR_O,DIR_D);
+console.log(FRAG_LIST);
+
